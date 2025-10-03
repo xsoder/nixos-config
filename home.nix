5e29adf3d4e2b5d0990430be84b56a4833e1eb49
xsoder <tadihailukebe@gmail.com>
2025-10-03 04:18:37 -0400

    FZF fixing path

--- home.nix
+++ home.nix
@@ -13,6 +13,7 @@
     ".config/dunst/dunstrc".source = ./dotfiles/dunst/dunstrc;
     ".vimrc".source = ./dotfiles/vimrc;
     ".vim/colors.vim".source = ./dotfiles/vim/colors.vim;
+    ".vim/fzf.vim".source = ./dotfiles/vim/fzf.vim;
     ".vim/keybinds.vim".source = ./dotfiles/vim/keybinds.vim;
     ".vim/options.vim".source = ./dotfiles/vim/options.vim;
     ".vim/plugins.vim".source = ./dotfiles/vim/plugins.vim;
@@ -27,6 +28,9 @@
     i3status
     emacs
     alacritty
+    fzf
+    ripgrep
+    fd
     discord
     btop
     scrot
